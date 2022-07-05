4e3f8aa581c07b87b253f1c25cb9313b28873c2f
smirnovsky <happy280190@gmail.com>
2022-07-05 17:20:55 +0300

    2048 finished. not stylised

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDisplay = document.getElementById('result')
     const width = 4
     let squares = []
+    let score = 0
 
     function createBoard() {
         for (let i = 0; i < width * width; i++) {
@@ -21,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let randomNumber = Math.floor(Math.random() * squares.length)
         if (squares[randomNumber].innerHTML == 0) {
             squares[randomNumber].innerHTML = 2
+            checkForGameOver()
         } else generate()
     }
 
@@ -120,6 +122,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
                 squares[i].innerHTML = combinedTotal
                 squares[i+1].innerHTML = 0
+                score += combinedTotal
+                scoreDisplay.innerHTML = score
             }
         }
         checkForWin()
@@ -128,9 +132,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function combineColumn() {
         for (let i=0; i<12; i++) {
             if (squares[i].innerHTML === squares[i+width].innerHTML) {
-                let combinedTotal = parseInt(squares[i+width].innerHTML) + parseInt(squares[i+1].innerHTML)
+                let combinedTotal = parseInt(squares[i+width].innerHTML) + parseInt(squares[i+width].innerHTML)
                 squares[i].innerHTML = combinedTotal
                 squares[i+width].innerHTML = 0
+                score += combinedTotal
+                scoreDisplay.innerHTML = score
             }
         }
         checkForWin()
@@ -180,10 +186,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function checkForWin() {
         for (let i=0; i < squares.length; i++) {
-            if (squares[i].innerHTML == 2048)
+            if (squares[i].innerHTML == 2048) {
                 resultDisplay.innerHTML = 'You Win!'
                 document.removeEventListener('keyup', control)
+            }    
         }
+    }
+
+    function checkForGameOver() {
+         let zeros = 0
+         for (let i = 0; i < squares.length; i++) {
+            if (squares[i].innerHTML == 0) {
+                zeros++
+            }
+         }
+         if (zeros === 0) {
+             resultDisplay.innerHTML == 'You lose'
+             document.removeEventListener('keyup', control)
+         }
     }
 })
 
